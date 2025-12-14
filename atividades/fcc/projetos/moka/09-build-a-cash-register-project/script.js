24d589d618c431ddcbbcc5906114cdc714ef2c6e
Moka <25541880+moacirsouza@users.noreply.github.com>
2025-12-14 13:13:30 -0300

    Consolida alterações antes do refatoramento geral
    1. Remove a vírgula extra contida na variável 'cid'. A vírgula criava um item
    extra no array, o que pode ter ocasionado o bug que gerava um valor indefinido
    quando a função de conversão monetária era chamada múltiplas vezes seguidas
    2. Cria a variável 'message' para separar as mensagens informativas do texto
    que contém o troco do usuário
    3. Renomeia o array auxiliar 'currencyUnits' para 'monetaryUnits', movendo-o
    para fora da função 'changeCalculator'
    4. Adiciona um zero (0) à chamada do 'map' na função 'currencyConverter', a fim
    de deixar mais explícito o uso do valor em formato decimal (float)
    5. Corrige a manipulação do valor da variável 'givenCash', que é passada como
    parâmetro para a função 'changeCalculator'. Anteriormente ela recebia o objeto
    'input text' com o valor passado pelo usuário, mas a manipulação de dentro da
    função ignorava esse fato e reassociava desnecessariamente o valor de
    'givenCash' ao parâmetro 'value' do objeto.
    6. Troca o uso da variável 'status' por 'message' ao longo do código
    7. Cria um bloco de comentários indicando um dos possíveis locais onde o bug do
    cálculo do troco pode estar. No entanto, se considerarmos a correção do item 01
    desta lista, não é possível definir com clareza, se o bug realmente está no
    local determinado pelos comentários. De qualquer maneira, o bloco foi mantido,
    uma vez que a questão ainda não foi completamente resolvida. No futuro, quando
    o bug for resolvido, ele poderá ser apagado

--- atividades/fcc/projetos/moka/09-build-a-cash-register-project/script.js
+++ atividades/fcc/projetos/moka/09-build-a-cash-register-project/script.js
@@ -1,4 +1,5 @@
-let price = 3.26;
+let price = 5.00;
+//let price = 3.26;
 
 let cid = [
   ['PENNY', 1.01],
@@ -9,16 +10,21 @@ let cid = [
   ['FIVE', 55],
   ['TEN', 20],
   ['TWENTY', 60],
-  ['ONE HUNDRED', 100],
+  ['ONE HUNDRED', 100]
 ];
 
+
+// Variables related to the various HTML elements
 const cashInput = document.getElementById("cash");
 const button = document.getElementById("purchase-btn");
 const changeDue = document.getElementById("change-due");
 const priceDisplay = document.getElementById("price-display");
 const drawerDisplay = document.getElementById("drawer");
+const message = document.getElementById("message");
 
-priceDisplay.innerHTML = `<strong>PRICE:</strong> ${price}`;
+// Auxiliary array with each monetary unit, already multiplied by 100 and
+// reversed in comparison to the original 'cid'
+const monetaryUnits = [ 10000, 2000, 1000, 500, 100, 25, 10, 5, 1 ]
 
 /**
  * TODO: Document this later
@@ -33,10 +39,13 @@ const currencyConverter = (number, outputDisplay) => {
   const convertedResult = number
                           .toString()
                           .split('.')
-                          .map((e, i) => i===0?e:`.${e}`)
+                          .map((e, i) => i===0?e:`0.${e}`)
                           .reduce((accumulator, currentValue) =>
-                                   parseInt(accumulator) + parseFloat(currentValue)*100, 0);
+                                   parseInt(accumulator)
+                                   + parseFloat(currentValue)*100, 0);
 
+
+  //console.log(`Converti ${number} para ${convertedResult}`);
   let convertedOutput = undefined;
 
   if(outputDisplay === 0){
@@ -48,28 +57,44 @@ const currencyConverter = (number, outputDisplay) => {
   return convertedOutput;
 }
 
-// console.log(currencyConverter('a', 1));
+let breakCid = cid.reduce((acc, cv) => acc + currencyConverter(cv[1], 0), 0);
+let toEmptyCid = currencyConverter(breakCid, 1) + price;
 
-// Auxiliary array with each currency unit, already multiplied by 100
-const currencyUnits = [ 10000, 2000, 1000, 500, 100, 25, 10, 5, 1 ]
+// priceDisplay.innerHTML = `<strong>PRICE:</strong> <br /> ${price} <br /> 338.67`;
+priceDisplay.innerHTML = `${price}<br />${toEmptyCid}`;
+cashInput.value=toEmptyCid;
 
 /**
  * Calculates the change due
+ *
+ * itemPrice (float): Value of the purchased item, passed a regular float value
+ * cidValue (array): 'cid' bidimensional array containing the monetary units
+ * names and sum of each unity in the cash register drawer
+ * givenCash (string): User input value
  */
 const changeCalculator = (itemPrice, cidValue, givenCash) => {
 
-  givenCash = currencyConverter(parseFloat(cashInput.value), 0);
-  let status = "OPEN";
+  givenCash = currencyConverter(parseFloat(givenCash), 0);
+  message.innerHTML = "";
 
   if(isNaN(givenCash)){
-    changeDue.innerHTML = "Please enter a valid monetary value.";
+    message.innerHTML += "OPEN: Please enter a valid monetary value.";
     cashInput.value = "";
     return;
   }
   
+  // TODO: O problema está aqui! O CID é reprocessado todas as vezes que a 
+  // função changeCalculator é chamada, portanto, um valor de 0.01 vira 1 na
+  // primeira chamada, depois 100 na segunta, 10000 na terceira e assim por
+  // diante. Esta conversão para inteiro só deve acontecer UMA VEZ. Aqui dentro
+  // deve-se apenas subtrair o valor do troco, seguindo a regra determinada
+  // pelo FCC
+
   // Creates a reversed copy of the cid array with all its numeric
   // values multiplied by 100
-  let cidInteger = cidValue.map(e => [e[0], currencyConverter(e[1], 0)]).toReversed();
+  let cidInteger = cidValue
+                   .map(e => [e[0], currencyConverter(e[1], 0)])
+                   .toReversed();
     
   const productPrice = currencyConverter(itemPrice, 0);
   
@@ -80,7 +105,8 @@ const changeCalculator = (itemPrice, cidValue, givenCash) => {
   }
 
   if(givenCash === productPrice){
-    changeDue.innerHTML = "No change due - customer paid with exact cash";
+    message.innerHTML = "No change due - customer paid with exact cash";
+    //changeDue.innerHTML = "No change due - customer paid with exact cash";
     return;
   }
 
@@ -90,10 +116,10 @@ const changeCalculator = (itemPrice, cidValue, givenCash) => {
                           accumulator + currentValue[1], 0);
 
   if(change === totalCID){
-    status = "CLOSED";
+    message.innerHTML = "CLOSED";
   }
 
-  if(status === "INSUFFICIENT_FUNDS"){
+  if(message.innerHTML === "INSUFFICIENT_FUNDS"){
     return;
   }
 
@@ -104,21 +130,21 @@ const changeCalculator = (itemPrice, cidValue, givenCash) => {
   const result = [];
   let partialResult;
   let possibleChange = cidInteger
-                       .filter((_, i) => currencyUnits[i] <= change)
+                       .filter((_, i) => monetaryUnits[i] <= change)
                        .reduce((acc, curr) => acc + curr[1], 0);
 
   for(let index = 0; index < cidInteger.length; index++){
     if(possibleChange < change){
-      status = "INSUFFICIENT_FUNDS";
-    }else if(change >= currencyUnits[index] && cidInteger[index][1] > 0){
+      message.innerHTML = "INSUFFICIENT_FUNDS";
+    }else if(change >= monetaryUnits[index] && cidInteger[index][1] > 0){
       if(change >= cidInteger[index][1]){
         change -= cidInteger[index][1];
         partialResult = cidInteger[index][1];
         cidInteger[index][1] = 0;
       }else{
-        partialResult = Math.floor(change/currencyUnits[index])*currencyUnits[index];
+        partialResult = Math.floor(change/monetaryUnits[index])*monetaryUnits[index];
         cidInteger[index][1] -= partialResult;
-        change %= currencyUnits[index];
+        change %= monetaryUnits[index];
       }
       result.push([cidInteger[index][0], currencyConverter(partialResult, 1)]);
     }
@@ -128,6 +154,12 @@ const changeCalculator = (itemPrice, cidValue, givenCash) => {
     }
   }
   
+  /*
+  console.log("CID INTEGER: \n" + cidInteger);
+  console.log("CID VALUE: \n" + cidValue);
+  return;
+  */
+
   cid = cidInteger.map(e => [e[0], currencyConverter(e[1], 1)]).toReversed();
 // 1. título - E o colspan? Não pode ser sempre 3!
 // 2. quantidade de 
@@ -157,12 +189,12 @@ drawerDisplay.innerHTML = cidTable;
 };
 
 button.addEventListener("click", () => {
-  changeCalculator(price, cid, cashInput);
+  changeCalculator(price, cid, cashInput.value);
 });
 
 cashInput.addEventListener("keydown", (e) => {
   if(e.key === "Enter"){
-    changeCalculator(price, cid, cashInput);
+    changeCalculator(price, cid, cashInput.value);
   }
 });
 
